d71f5b4e96a19f0e2cdfb2119cb6554060d855e9
Nick Riemondi <nick@riemondi.com>
2019-01-29 22:11:01 +0000

    figuring out firestore timestamps, got a basic start/stop task going

--- src/components/tasks/Task.js
+++ src/components/tasks/Task.js
@@ -1,29 +1,55 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+import { firestoreConnect } from 'react-redux-firebase';
 
 import LoggedTime from './LoggedTime';
 
-export default class Task extends Component {
+class Task extends Component {
   state = {
     isActive: null,
     startedDateStr: null
   };
 
+  constructor(props) {
+    super(props);
+    this.handleRowClick = this.handleRowClick.bind(this);
+  }
+
   componentDidMount() {
     const { task } = this.props;
     this.setState({
       isActive: task.started !== null,
-      startedDateStr:
-        task.started === null
-          ? null
-          : new Date(task.started.toDate()).toDateString()
+      startedDateStr: this.getStartedDateStr(task.started)
     });
+  }
+
+  getStartedDateStr(started) {
+    return started === null ? null : new Date(started.toDate()).toDateString();
+  }
+
+  handleRowClick() {
+    const { task, firestore } = this.props;
+    const isActive = !this.state.isActive;
+    const started = isActive ? new Date() : null;
+    const startedDateStr = isActive ? started.toDateString() : null;
+
+    const taskUpdate = {
+      started
+    };
+
+    firestore.update({ collection: 'tasks', doc: task.id }, taskUpdate);
+
+    this.setState({ isActive, started, startedDateStr });
   }
 
   render() {
     const { task } = this.props;
 
     return (
-      <div className="row">
+      <div
+        className={'row' + (this.state.isActive ? ' bg-primary' : '')}
+        onClick={this.handleRowClick}
+      >
         <div className="col col-1" />
         <div className="col col-4">{task.name}</div>
         <div className="col col-3">
@@ -35,3 +61,5 @@ export default class Task extends Component {
     );
   }
 }
+
+export default firestoreConnect()(Task);
